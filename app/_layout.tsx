ea8a82be727e09948b53dec92d85fd492539b22b
uxearwenvcmsqg <vladkim_88@icloud.com>
2024-09-25 00:03:42 +0500

    fix

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -6,8 +6,6 @@ import { useEffect } from 'react';
 import 'react-native-reanimated';
 
 import { useColorScheme } from '@/hooks/useColorScheme';
-////iwhafoihqlwg
-// Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
